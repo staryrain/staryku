379145f8b05d472906f2951ca85e8eb9ef8af533
staryrain <126848217+staryrain@users.noreply.github.com>
2023-03-21 23:35:02 +0800

    Add files via upload

---
+++ Assignment4/Clock.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_clock
+{
+    public delegate void ClockHander(object sender, ClockEventArgs arges);
+    public class ClockEventArgs {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+
+    public class ClockTick
+    {
+        public event ClockHander Tick;
+        public void CreatTick(int x,int y,int z)
+        {
+            Console.WriteLine($"Clock time is {x} hour,{y} minute,{z} second");
+            ClockEventArgs args = new ClockEventArgs() { Hour = x, Minute = y, Second = z };
+            Tick(this, args);
+        }
+        
+    }
+    public class ClockAlarm
+    {
+        public event ClockHander Alarm;
+        public void Creatalarm(int x, int y, int z)
+        {
+            Console.WriteLine($"Time now is {x} hour,{y} minute,{z} second");
+            ClockEventArgs args = new ClockEventArgs() { Hour = x, Minute = y, Second = z };
+            Alarm(this, args);
+        }
+
+    }
+
+    public class ClockConsole
+    {
+        public ClockTick clocktick=new ClockTick();
+        public ClockAlarm clockalarm=new ClockAlarm();
+        public ClockConsole()
+        {
+            clocktick.Tick += new ClockHander(Ckt_Tick);
+            clockalarm.Alarm += new ClockHander(Ckt_Alarm);
+        }
+        void Ckt_Tick(object sender, ClockEventArgs args) {
+            Console.WriteLine("Clock is beginning");
+        }
+        void Ckt_Alarm(object sender, ClockEventArgs args)
+        {
+            Console.WriteLine("Clock is alarming");
+        }
+
+    }
+    class program {
+        static void Main(string[] args)
+        {
+            ClockConsole console1 = new ClockConsole();
+            console1.clocktick.CreatTick(6,0,0);
+            console1.clockalarm.Creatalarm(6,0,0);
+            Console.ReadLine();
+        }
+     }
+    
+}
---
+++ Assignment4/FindMaxMinAndAdd.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project求值
+{
+    public class Node<T>
+    {
+       public Node<T> Next { get; set; }
+        public T Data {  get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T> {
+        private Node<T> head;
+        private Node<T> tail;
+         
+        public GenericList()
+        {
+            tail=head=null;
+        } 
+         public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t) { 
+        Node<T> n =new Node<T>(t);
+        if(tail==null) {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> current = head;
+
+            while (current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            list.ForEach(x => Console.WriteLine(x));
+
+            int sum = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            list.ForEach(x =>
+            {
+                sum += x;
+                max = Math.Max(max, x);
+                min = Math.Min(min, x);
+            });
+
+            Console.WriteLine("Sum= " + sum);
+            Console.WriteLine("Max= " + max);
+            Console.WriteLine("Min= " + min);
+            Console.ReadLine();
+        }
+    }
+}
